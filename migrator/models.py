440d9442ca3b975e34b35945294148bf45f49d7f
Ben Kuhn <ben.s.kuhn@gmail.com>
2020-12-27 11:21:24 -0500

    Repository parsing

--- migrator/models.py
+++ migrator/models.py
@@ -1,9 +1,59 @@
 from __future__ import annotations
 
+import os.path
+import glob
+import yaml
 import abc
 from pydantic import BaseModel, root_validator
 from dataclasses import dataclass, field
 from typing import List, Union, Optional
+
+def get_revision_number(filename):
+    return int(os.path.basename(filename).split("-", 1)[0])
+
+def load_yaml(fname):
+    with open(fname) as f:
+        return yaml.safe_load(f.read())
+
+@dataclass
+class Repo:
+    revisions: List[Revision]
+
+    @staticmethod
+    def parse(dir: str) -> Repo:
+        assert os.path.isdir(dir)
+        revisions = []
+        for f in glob.glob(os.path.join(dir, "*.yml")):
+            revisions.append(Revision.parse(f))
+        revisions.sort(key=lambda r: r.number)
+        # check no missing revisions
+        assert revisions[0].number == 1
+        for (cur, next) in zip(revisions[:-1], revisions[1:]):
+            assert cur.number + 1 == next.number
+        return Repo(revisions)
+
+@dataclass
+class Revision:
+    # TODO validate
+    number: int
+    migration_filename: str
+
+    @property
+    def schema_filename(self):
+        return os.path.join(
+            os.path.dirname(self.migration_filename),
+            f"{self.number}-schema.sql"
+        )
+
+    @staticmethod
+    def parse(filename: str) -> Revision:
+        assert os.path.isfile(filename)
+        number = get_revision_number(filename)
+        return Revision(number, filename)
+
+    @property
+    def migration(self) -> Migration:
+        return Migration.parse_obj(load_yaml(self.migration_filename))
 
 class Migration(BaseModel):
     revision: int
--- tests/test_models.py
+++ tests/test_models.py
@@ -6,7 +6,8 @@ def load_yaml(fname):
         return yaml.safe_load(f.read())
         
 def test_basic_parse():
-    m = models.Migration.parse_obj(load_yaml("test/migrations/1-create-table.yml"))
+    r = models.Repo.parse("test/migrations")
+    m = r.revisions[0].migration
     assert m.post_deploy == []
     [step1] = m.pre_deploy
     assert step1.run_ddl is not None
