c65e2fa87521a64f78460e8c88b108489f47d99b
Ben Kuhn <ben.s.kuhn@gmail.com>
2021-01-08 15:53:51 -0600

    Fix test

--- tests/test_up.py
+++ tests/test_up.py
@@ -60,3 +60,4 @@ def ctx(db_url: str) -> Context:
 def test_init(ctx: Context) -> None:
     ctx.ui.respond_yes_no(text.ASK_TO_INITIALIZE_DB, "y")
     up.up(ctx)
+    ctx.db().cur.execute("select u_id, email, mobile from users")
