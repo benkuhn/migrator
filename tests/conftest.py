e8c7c565c779f62ccd8f16b423c96ccc390696b2
Ben Kuhn <ben.s.kuhn@gmail.com>
2021-01-09 10:34:39 -0600

    Speed up tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import psycopg2
 import pytest
 
 from migrator.commands import Context
-from migrator.db import temp_db_url
+from migrator.db import temp_db_url, NAME
 from tests.fakes import FakeUserInterface
 
 
@@ -17,23 +17,25 @@ def control_conn():
     return conn
 
 
-@pytest.fixture
-def db_url(control_conn) -> Iterator[str]:
-    with temp_db_url(control_conn) as url:
-        yield url
+RESET_DDL = f"""
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+DROP SCHEMA IF EXISTS {NAME} CASCADE; 
+"""
 
-
 @pytest.fixture
-def ctx(db_url: str) -> Context:
+def ctx(control_conn) -> Context:
     old_dir = os.getcwd()
     os.chdir("test")
     try:
         ctx = Context(
             "migrator.yml",
-            db_url,
+            os.environ["DATABASE_URL"],
             FakeUserInterface()
         )
         with contextlib.closing(ctx):
             yield ctx
     finally:
+        with control_conn.cursor() as cur:
+            cur.execute(RESET_DDL)
         os.chdir(old_dir)
