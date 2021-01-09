62599310edcb1a6e51890c90c5ab83f0e247a6fc
Ben Kuhn <ben.s.kuhn@gmail.com>
2021-01-09 06:59:35 -0600

    Remember to add post_deploy

--- migrator/commands/revision.py
+++ migrator/commands/revision.py
@@ -51,10 +51,8 @@ def revision(ctx: Context, message: str) -> None:
 
     migration = {
         "message": message,
-        "pre_deploy": [
-            step.dict()
-            for step in pre_deploy
-        ]
+        "pre_deploy": [step.dict() for step in pre_deploy],
+        "post_deploy": [step.dict() for step in post_deploy]
     }
 
     with ctx.ui.open(migration_path, "w") as f:
